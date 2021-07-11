4cbe5be8c959f164d245ba36bebdf24efda7edf5
Gabriel <tetradog@gmail.com>
2021-07-11 08:35:36 +0200

    commit

--- CheckPuya.Net/Capitulo.cs
+++ CheckPuya.Net/Capitulo.cs
@@ -6,21 +6,36 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CypherExample;
 using Gabriel.Cat.S.Check;
+using Gabriel.Cat.S.Extension;
+using HtmlAgilityPack;
 
 namespace CheckPuya.Net
 {
-    public class Capitulo:IFileMega
+    public class Capitulo : IFileMega
     {
         public string Name { get; set; }
         public Uri Page { get; set; }
         public Uri Picture { get; set; }
         public Uri Mega1080 { get; set; }
         public Uri Mega720 { get; set; }
-        public string[] GetLinksMega() => new string[] {Mega1080.GetLinkMega().AbsoluteUri,Mega720.GetLinkMega().AbsoluteUri };
+        public string[] GetLinksMega() => new string[] { Mega1080.GetLinkMega().AbsoluteUri, Mega720.GetLinkMega().AbsoluteUri };
 
         public static IEnumerable<Capitulo> GetCapitulos(Uri webPuya)
         {
-            throw new NotImplementedException();
+            string html = webPuya.DownloadString();
+            HtmlDocument doc = new HtmlDocument().LoadString(html);
+            return doc.GetElementbyId("content").GetByTagName("article").Select(nodoArticle =>
+            {
+                Capitulo capitulo = new Capitulo();
+                HtmlNode nodoLinks = nodoArticle.GetByTagName("div").First();
+                capitulo.Page = new Uri(nodoArticle.GetByTagName("h2").First().GetByTagName("a").First().Attributes["href"].Value);
+                capitulo.Picture = new Uri(nodoArticle.GetByTagName("img").First().Attributes["src"].Value);
+                capitulo.Name = nodoArticle.GetByTagName("i").First().GetByTagName("a").First().InnerText;
+                capitulo.Mega1080 = new Uri(nodoLinks.GetByTagName("div").First().GetByTagName("a").Last().Attributes["href"].Value);
+                capitulo.Mega720 = new Uri(nodoLinks.GetByTagName("div").Last().GetByTagName("a").Last().Attributes["href"].Value);
+                return capitulo;
+            });
+
         }
 
 
@@ -29,7 +44,8 @@ namespace CheckPuya.Net
     {
         public static Uri GetLinkMega(this Uri linkMega)
         {
-            return new Uri(linkMega.DecryptUri((html)=> {
+            return new Uri(linkMega.DecryptUri((html) =>
+            {
                 string data;
                 Regex regex = new Regex("(<INPUT[^>]*>)");
                 Match match = regex.Match(html).NextMatch();//quito el primero que es el source
